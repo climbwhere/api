aa7814e25915a90274663cc1c7fe27b9f1fce317
Ravern Koh <ravernkoh@gmail.com>
2021-04-22 14:54:33 +0800

    remove puppeteer import

--- src/scraper/context.ts
+++ src/scraper/context.ts
@@ -1,9 +1,7 @@
 import type { AxiosInstance } from "axios";
 import type { Knex } from "knex";
-import type { Browser } from "puppeteer";
 
 export type Context = {
   db: Knex;
-  // browser: Browser;
   http: AxiosInstance;
 };
