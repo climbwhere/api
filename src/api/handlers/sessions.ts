1666d16b35377abe19542fba8fe1d51c90b55997
Ravern Koh <ravernkoh@gmail.com>
2021-06-27 09:46:16 +0800

    generate sessions from snapshot

--- src/api/handlers/sessions.ts
+++ src/api/handlers/sessions.ts
@@ -1,43 +1,63 @@
+import { flatten, keyBy, keys, map, orderBy } from "lodash";
+import getLatestSnapshot from "../../db/queries/getLatestSnapshot";
+
+import type { Gym } from "../../db/models";
 import type { Context } from "../context";
 import type { Handler } from "./types";
 
+type ResponseBody = {
+  data: Session[];
+};
+
+type Session = {
+  id: string;
+  starts_at: Date;
+  ends_at: Date;
+  spaces: number;
+  gym: {
+    id: string;
+    slug: string;
+    name: string;
+    address?: string;
+    phone?: string;
+    email?: string;
+  };
+};
+
 export const index: Handler = (ctx: Context) => async (req, res) => {
-  const rawSessions = await ctx
-    .db("sessions")
-    .join("gyms", "gyms.id", "sessions.gym_id")
-    .where("sessions.starts_at", ">", ctx.db.raw("NOW()"))
-    .orderBy("sessions.starts_at", "asc")
-    .select(
-      "sessions.id",
-      "sessions.starts_at",
-      "sessions.ends_at",
-      "sessions.spaces",
-      "sessions.gym_id",
-      "gyms.slug AS gym_slug",
-      "gyms.name AS gym_name",
-      "gyms.address AS gym_address",
-      "gyms.phone AS gym_phone",
-      "gyms.email AS gym_email",
-    );
+  const snapshot = await getLatestSnapshot(ctx.db);
 
-  const sessions = rawSessions.map((session) => ({
-    id: session.id,
-    starts_at: session.starts_at,
-    ends_at: session.ends_at,
-    spaces: session.spaces,
-    gym: {
-      id: session.gym_id,
-      slug: session.gym_slug,
-      name: session.gym_name,
-      address: session.gym_address,
-      phone: session.gym_phone,
-      email: session.gym_email,
-    },
-  }));
+  const gyms = keyBy(
+    await Promise.all<Gym>(
+      keys(snapshot.data.sessions).map(
+        (slug): Promise<Gym> => ctx.db("gyms").where({ slug }).first(),
+      ),
+    ),
+    "slug",
+  );
 
-  res.json({
+  const sessions = orderBy(
+    flatten(
+      map(snapshot.data.sessions, (result, gymSlug): Session[] => {
+        if (result.error) {
+          return [];
+        }
+        const gym = gyms[gymSlug];
+        return map(result.data, (session) => ({
+          ...session,
+          gym_id: undefined,
+          gym,
+        }));
+      }),
+    ),
+    "starts_at",
+    "asc",
+  );
+
+  const body: ResponseBody = {
     data: sessions,
-  });
+  };
+  res.json(body);
 };
 
 export default {
