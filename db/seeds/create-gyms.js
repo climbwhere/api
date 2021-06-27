6caf1e4814439fcdd064a33ec53433381e639d42
Ravern Koh <ravernkoh@gmail.com>
2021-06-27 09:05:01 +0800

    refactor seeds for gyms from sql to js

---
+++ db/seeds/create-gyms.js
@@ -0,0 +1,16 @@
+exports.seed = function (knex) {
+  return knex("gyms").insert([
+    { name: "BFF Climb", slug: "bff" },
+    { name: "BM Downtown", slug: "boulder-movement-downtown" },
+    { name: "BM Rochor", slug: "boulder-movement-rochor" },
+    { name: "BM Tai Seng", slug: "boulder-movement-tai-seng" },
+    { name: "Boulder Planet", slug: "boulder-planet" },
+    { name: "Boulder World", slug: "boulder-world" },
+    { name: "Boulder+", slug: "boulder-plus" },
+    { name: "Fit Bloc", slug: "fitbloc" },
+    // { name: "Lighthouse", slug: "lighthouse" },
+    { name: "OYEYO", slug: "oyeyo" },
+    { name: "The Rock School", slug: "the-rock-school" },
+    { name: "Z-Vertigo", slug: "z-vertigo" },
+  ]);
+};
