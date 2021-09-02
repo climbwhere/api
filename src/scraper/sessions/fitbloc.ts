382bbc1beae56cb127e03d2f53c30492190ac388
Ravern Koh <ravernkoh@gmail.com>
2021-09-02 20:46:07 +0800

    fix fitbloc

--- src/scraper/sessions/fitbloc.ts
+++ src/scraper/sessions/fitbloc.ts
@@ -19,61 +19,66 @@ const scrape = async (ctx: Context, slug: string): Promise<Session[]> => {
   }
 
   const sessions = [];
-  
+
+  for (let i = 0; i < 8; i++) {
     const startDate = moment()
       .startOf("day")
+      .add(i, "days")
       .format("YYYY-MM-DD");
 
-  const res = await ctx.http.get(
-    "https://widgets.mindbodyonline.com/widgets/schedules/77427/load_markup",
-    {
-      params: {
-        "options[start_date]": startDate,
+    const res = await ctx.http.get(
+      "https://widgets.mindbodyonline.com/widgets/schedules/77427/load_markup",
+      {
+        params: {
+          "options[start_date]": startDate,
+        },
       },
-    },
-  );
+    );
 
-  const $ = cheerio.load(res.data.class_sessions);
+    const $ = cheerio.load(res.data.class_sessions);
 
-  if ($(".bw-widget__empty.is-visible").length !== 0) {
-    return [];
-  }
-
-  const startIndex =
-    res.data.class_sessions.indexOf("scheduleData = ") +
-    "scheduleData = ".length;
-  const endIndex = res.data.class_sessions.indexOf("$.event.trigger");
-  const scheduleData = JSON.parse(
-    res.data.class_sessions.substring(startIndex, endIndex),
-  );
-
-  $(".bw-session").each((index, sessionElem) => {
-    const name = $(".bw-session__name", sessionElem).text();
-
-    if (!name.includes("Daily Entry - Gym Entry")) {
-      return;
+    if ($(".bw-widget__empty.is-visible").length !== 0) {
+      return [];
     }
 
-    const sessionId = $(sessionElem).attr("data-bw-widget-mbo-class-id");
-    const $$ = cheerio.load(scheduleData.contents[sessionId].classAvailability);
+    const startIndex =
+      res.data.class_sessions.indexOf("scheduleData = ") +
+      "scheduleData = ".length;
+    const endIndex = res.data.class_sessions.indexOf("$.event.trigger");
+    const scheduleData = JSON.parse(
+      res.data.class_sessions.substring(startIndex, endIndex),
+    );
 
-    const slotsElem = $$(".hc_availability");
-    const waitlistElem = $$(".hc_waitlist");
+    $(".bw-session").each((index, sessionElem) => {
+      const name = $(".bw-session__name", sessionElem).text();
 
-    let spaces = 0;
-    if (waitlistElem.length === 0) {
-      if (slotsElem.length === 0) {
+      if (!name.includes("Gym Entry")) {
         return;
       }
-      spaces = parseInt(slotsElem.text().trim().slice(0, 2), 10);
-    }
 
-    sessions.push({
-      starts_at: $(".hc_starttime", sessionElem).attr("datetime"),
-      ends_at: $(".hc_endtime", sessionElem).attr("datetime"),
-      spaces,
+      const sessionId = $(sessionElem).attr("data-bw-widget-mbo-class-id");
+      const $$ = cheerio.load(
+        scheduleData.contents[sessionId].classAvailability,
+      );
+
+      const slotsElem = $$(".hc_availability");
+      const waitlistElem = $$(".hc_waitlist");
+
+      let spaces = 0;
+      if (waitlistElem.length === 0) {
+        if (slotsElem.length === 0) {
+          return;
+        }
+        spaces = parseInt(slotsElem.text().trim().slice(0, 2), 10);
+      }
+
+      sessions.push({
+        starts_at: $(".hc_starttime", sessionElem).attr("datetime"),
+        ends_at: $(".hc_endtime", sessionElem).attr("datetime"),
+        spaces,
+      });
     });
-  });
+  }
 
   const fitblocSessions = sessions.map<FitblocSession>((session) => ({
     starts_at: moment(session.starts_at).toDate(),
