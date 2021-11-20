6353abe81766e2d5df7d58076302357efc56e378
Sean <seanlimdev856@gmail.com>
2021-11-20 19:43:04 +0800

    cache snapshot response

--- src/api/handlers/snapshots.ts
+++ src/api/handlers/snapshots.ts
@@ -1,14 +1,27 @@
 import getLatestSnapshot from "../../db/queries/getLatestSnapshot";
 import type { Context } from "../context";
 import type { Handler } from "./types";
+import cache from "memory-cache";
 
 export const get: Handler = (ctx: Context) => async (req, res) => {
   const { id } = req.params;
+  let snapshot;
 
-  const snapshot =
-    id === "latest"
-      ? await getLatestSnapshot(ctx.db)
-      : await ctx.db("snapshots").where({ id }).first();
+  const cached = cache.get(id);
+
+  if (cached !== null) {
+    snapshot = cached;
+  } else {
+    snapshot =
+      id === "latest"
+        ? await getLatestSnapshot(ctx.db)
+        : await ctx.db("snapshots").where({ id }).first();
+
+    cache.put(id, snapshot, 120000, () => {
+      // purge after 2 minutes
+      cache.clear();
+    });
+  }
 
   res.json({
     data: snapshot,
